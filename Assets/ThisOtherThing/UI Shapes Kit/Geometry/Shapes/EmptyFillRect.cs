855ce1a1b18cc86ecb0aed4589cf679696958c1e
Matthew Holtzem <mjholtzem@gmail.com>
2026-03-08 09:49:13 -0600

    adding canvasrenderer to empty fill rect

--- Assets/ThisOtherThing/UI Shapes Kit/Geometry/Shapes/EmptyFillRect.cs
+++ Assets/ThisOtherThing/UI Shapes Kit/Geometry/Shapes/EmptyFillRect.cs
@@ -3,16 +3,16 @@ using UnityEngine.UI;
 
 namespace ThisOtherThing.UI.Shapes
 {
+    [RequireComponent(typeof(CanvasRenderer))]
+    [AddComponentMenu("UI/Shapes/Empty Fill Rect", 200)]
+    public class EmptyFillRect : Graphic
+    {
+        public override void SetMaterialDirty() { return; }
+        public override void SetVerticesDirty() { return; }
 
-	[AddComponentMenu("UI/Shapes/Empty Fill Rect", 200)]
-	public class EmptyFillRect : Graphic
-	{
-		public override void SetMaterialDirty() { return; }
-		public override void SetVerticesDirty() { return; }
-
-		protected override void OnPopulateMesh(VertexHelper vh)
-		{
-			vh.Clear();
-		}
-	}
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+        }
+    }
 }
